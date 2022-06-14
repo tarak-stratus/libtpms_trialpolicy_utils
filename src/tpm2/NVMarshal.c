ae7f82d2ab1320cc881c0f4abd8a96cb4f875f91
Stefan Berger <stefanb@linux.ibm.com>
2022-06-14 15:21:25 -0400

    tpm2: Write the profile as part of PERMANENT_ALL state v4
    Write the profile as part of PERMANENT_ALL state v4 and also read it from
    the byte stream if the PERMANENT_ALL state has v4 or later.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- src/tpm2/NVMarshal.c
+++ src/tpm2/NVMarshal.c
@@ -293,6 +293,62 @@ TPM2B_PROOF_Unmarshal(TPM2B_PROOF *target, BYTE **buffer, INT32 *size)
     return rc;
 }
 
+/* Marshal a string including its terminating NUL byte. A NULL pointer will be
+ * marshalled with 0 bytes and will be unmarshalled as a NULL pointer again.
+ */
+static UINT16
+String_Marshal(const char *source, BYTE **buffer, INT32 *size)
+{
+    UINT16 written = 0;
+    UINT16 len = 0;
+
+    if (source)
+        len = (UINT16)strlen(source) + 1;
+    written += UINT16_Marshal(&len, buffer, size);
+
+    if (len > 0 && *size >= len) {
+        memcpy(*buffer, source, len);
+        *buffer += len;
+        *size -= len;
+
+        written += len;
+    }
+
+    return written;
+}
+
+static TPM_RC
+String_Unmarshal(char **target, BYTE **buffer, INT32 *size)
+{
+    TPM_RC rc = TPM_RC_SUCCESS;
+    UINT16 len;
+
+    *target = NULL;
+
+    if (rc == TPM_RC_SUCCESS) {
+        rc = UINT16_Unmarshal(&len, buffer, size);
+    }
+    if (rc == TPM_RC_SUCCESS) {
+        if (*size < len) {
+            rc = TPM_RC_INSUFFICIENT;
+        } else if (len > 0) {
+            *target = malloc(len);
+            if (!*target) {
+                rc = TPM_RC_MEMORY;
+            }
+        }
+    }
+    if (rc == TPM_RC_SUCCESS) {
+        if (len > 0) {
+            memcpy(*target, *buffer, len);
+            *buffer += len;
+            *size -= len;
+        }
+    }
+
+    return rc;
+}
+
 static TPM_RC
 UINT32_Unmarshal_Check(UINT32 *data, UINT32 exp, BYTE **buffer, INT32 *size,
                        const char *msg)
@@ -4779,7 +4835,7 @@ exit_size:
  * - indexOrderlyRAM  (NV_INDEX_RAM_DATA)
  * - NVRAM locations  (NV_USER_DYNAMIC)
  */
-#define PERSISTENT_ALL_VERSION 3
+#define PERSISTENT_ALL_VERSION 4
 #define PERSISTENT_ALL_MAGIC   0xab364723
 UINT32
 PERSISTENT_ALL_Marshal(BYTE **buffer, INT32 *size)
@@ -4793,6 +4849,7 @@ PERSISTENT_ALL_Marshal(BYTE **buffer, INT32 *size)
     BYTE indexOrderlyRam[sizeof(s_indexOrderlyRam)];
     BLOCK_SKIP_INIT;
     BOOL writeSuState;
+    const char *profileJSON = RuntimeProfileGetJSON(&g_RuntimeProfile);
 
     NvRead(&pd, NV_PERSISTENT_DATA, sizeof(pd));
     NvRead(&od, NV_ORDERLY_DATA, sizeof(od));
@@ -4802,9 +4859,20 @@ PERSISTENT_ALL_Marshal(BYTE **buffer, INT32 *size)
     /* indexOrderlyRam was never endianess-converted; so it's native */
     NvRead(indexOrderlyRam, NV_INDEX_RAM_DATA, sizeof(indexOrderlyRam));
 
-    written = NV_HEADER_Marshal(buffer, size,
-                                PERSISTENT_ALL_VERSION,
-                                PERSISTENT_ALL_MAGIC, 3);
+    /* If we previously read state that did not contain a profile then
+     * we don't want to upgrade the state unnecessarily but still write
+     * it as v3.
+     */
+    if (RuntimeProfileWasNullProfile(&g_RuntimeProfile)) {
+        written = NV_HEADER_Marshal(buffer, size,
+                                    3,
+                                    PERSISTENT_ALL_MAGIC, 3);
+    } else {
+        written = NV_HEADER_Marshal(buffer, size,
+                                    PERSISTENT_ALL_VERSION,
+                                    PERSISTENT_ALL_MAGIC, 4);
+        written += String_Marshal(profileJSON, buffer, size); // since v4
+    }
     written += PACompileConstants_Marshal(buffer, size);
     written += PERSISTENT_DATA_Marshal(&pd, buffer, size);
     written += ORDERLY_DATA_Marshal(&od, buffer, size);
@@ -4842,6 +4910,7 @@ PERSISTENT_ALL_Unmarshal(BYTE **buffer, INT32 *size)
     STATE_CLEAR_DATA scd;
     BYTE indexOrderlyRam[sizeof(s_indexOrderlyRam)];
     BOOL readSuState = false;
+    char *profileJSON = NULL;
 
     memset(&pd, 0, sizeof(pd));
     memset(&od, 0, sizeof(od));
@@ -4855,6 +4924,9 @@ PERSISTENT_ALL_Unmarshal(BYTE **buffer, INT32 *size)
                                  PERSISTENT_ALL_MAGIC);
     }
 
+    if (rc == TPM_RC_SUCCESS && hdr.version >= 4) {
+        rc = String_Unmarshal(&profileJSON, buffer, size);
+    }
     if (rc == TPM_RC_SUCCESS) {
         rc = PACompileConstants_Unmarshal(buffer, size);
     }
@@ -4908,7 +4980,10 @@ skip_future_versions:
         NvWrite(NV_STATE_RESET_DATA, sizeof(srd), &srd);
         NvWrite(NV_STATE_CLEAR_DATA, sizeof(scd), &scd);
         NvWrite(NV_INDEX_RAM_DATA, sizeof(indexOrderlyRam), indexOrderlyRam);
+        rc = RuntimeProfileSet(&g_RuntimeProfile, profileJSON, false);
     }
+
+    free(profileJSON);
 
     return rc;
 }
